665892657cf781b8e6b0180343feb5badeb69269
Ishkevin32 <ishimwekevin32@gmail.com>
2023-11-29 20:17:06 +0200

    First commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
