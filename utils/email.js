21258906102f94ff691b1525752f1c2faf3d033c
Ishkevin32 <ishimwekevin32@gmail.com>
2023-12-10 00:48:17 +0200

    Email now sending corretly in mailtrap.io fixed the bug with installing html-to-text@5.1.1

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-const htmToText = require('html-to-text');
+const htmlToText = require('html-to-text');
 
 module.exports = class Email {
   constructor(user, url) {
@@ -44,7 +44,7 @@ module.exports = class Email {
       to: this.to,
       subject,
       html,
-      text: htmToText.fromString(html)
+      text: htmlToText.fromString(html)
     };
 
     // 3) Create a transport and send the email
