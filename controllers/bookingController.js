d60b5711411de5e1cbca3164a639d87c8cc30762
Ishkevin32 <ishimwekevin32@gmail.com>
2024-02-02 22:00:07 +0200

    Refactoring the Backend removing all unnecessary files and modified some responses of the end points

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -13,11 +13,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    success_url: 'http://127.0.0.1:3004/api/v1/bookings/my-tours',
+    cancel_url: 'http://127.0.0.1:3004/api/v1/tours/',
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
@@ -27,11 +24,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [
-              `${req.protocol}://${req.get('host')}/img/tours/${
-                tour.imageCover
-              }`
-            ]
+            images: [`public/img/tours/${tour.imageCover}`]
           },
           unit_amount: tour.price * 100
         },
@@ -46,16 +39,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session
   });
 });
-
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is onle temporary, because it's UNSECURE: everyone can make bookings without paying
-//   const { tour, user, price } = req.query;
-
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split('?')[0]);
-// });
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
